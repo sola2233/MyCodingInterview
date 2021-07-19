dfcde19c5267b7c954d6803e7099542ee669becd
sola2233 <442785423@qq.com>
2021-07-20 00:44:35 +0800

    modify

--- 00_Solution/Solution.h
+++ 00_Solution/Solution.h
@@ -28,25 +28,10 @@ public:
     bool findNumberIn2DArray(vector<vector<int>>& matrix, int target);  // 04二维数组中找值
     string replaceSpace(string s);                                      // 05替换空格
     vector<int> reversePrint(ListNode* head);                           // 06反向打印链表
-    // TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder);   // 07建立二叉树
-public:
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        this->preorder = preorder;
-        for(int i = 0; i < inorder.size(); i++)
-            dic[inorder[i]] = i;
-        return recur(0, 0, inorder.size() - 1);
-    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder);   // 07建立二叉树
+    TreeNode* myBuildTree(const vector<int>& preorder, const vector<int>& inorder, int preorder_left, int preorder_right, int inorder_left, int inorder_right);
 private:
-    vector<int> preorder;
-    unordered_map<int, int> dic;
-    TreeNode* recur(int root, int left, int right) { 
-        if(left > right) return nullptr;                        // 递归终止
-        TreeNode* node = new TreeNode(preorder[root]);          // 建立根节点
-        int i = dic[preorder[root]];                            // 划分根节点、左子树、右子树
-        node->left = recur(root + 1, left, i - 1);              // 开启左子树递归
-        node->right = recur(root + i - left + 1, i + 1, right); // 开启右子树递归
-        return node;                                            // 回溯返回根节点
-    }
+    unordered_map<int, int> index;
 
 };
 
